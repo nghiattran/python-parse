009fdcc5792d3ca831b8dbc2fa425b192a470efd
NghiaTTran <nghiattran3@gmail.com>
2015-12-20 22:43:08 -0500

    add new features

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import\
-    Flask
+    Flask, render_template
 from flask_restful import\
     Api
 from flask.ext.cors import CORS
@@ -12,13 +12,25 @@ from src.controllers.user_controller import \
     AuthController, \
     UserActivationController
 import redis
+import jinja2
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='src/static')
+# set path to custom templates folder
+app.jinja_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader('src/templates'),
+])
+
+
+# set up redis
 app.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 app.debug = True
 
-api = Api(app, prefix="/api/")
+# allow CORS so that localhost:8000 can talk with localhost:9000
 CORS(app)
+
+# initial for api
+api = Api(app, prefix="/api/")
 
 api.add_resource(UsersController, 'users')
 api.add_resource(UserController, 'users/<string:object_id>')
@@ -32,6 +44,14 @@ api.add_resource(ResetpasswordController, 'resetpassword')
 api.add_resource(AuthController, 'auth')
 
 api.add_resource(UserActivationController, 'activate/<string:object_id>')
+
+@app.route('/activated/<string:object_id>')
+def activated(object_id):
+    return render_template("activated.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
